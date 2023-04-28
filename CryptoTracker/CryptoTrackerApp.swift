d53d631e75f72e655c166325caca5c86f8b7f5e3
emreozkok <emreozkok88@gmail.com>
2023-04-28 10:35:26 +0300

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Emre ÖZKÖK on 28.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
